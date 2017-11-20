a86324f7b18ae8c3878666d2d9b0eba633f9c0c7
Augusto Miguel Zeponi <augustozeponi@hotmail.com>
2017-11-20 20:15:24 -0200

    Criando a página principal com o Materialize

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -17,13 +17,17 @@
         <nav>
             <div class="nav-wrapper blue">
                 <div class="container">
-                    <a href="#!" class="brand-logo">Logo</a>
+                    <a href="#!" class="brand-logo">SiteDinâmico</a>
                     <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
                     <ul class="right hide-on-med-and-down">
                         <li><a href="#">Home</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Contato</a></li>
                     </ul>
                     <ul class="side-nav" id="mobile-demo">
                         <li><a href="#">Home</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Contato</a></li>
                     </ul>
                 </div>
             </div>
@@ -36,3 +40,4 @@
         <script src="{{asset('js/init.js')}}"></script>
     </body>
 </html>
+
