8e42218b1788c0563b6dcca165740ec4a04d4f20
Augusto Miguel Zeponi <augustozeponi@hotmail.com>
2017-12-15 22:36:53 -0200

    #4 - Cadastro de cidades

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -64,4 +64,11 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/admin/tipos/editar/{id}', ['as' => 'admin.tipos.editar', 'uses' => 'Admin\TipoController@editar']);
     Route::put('/admin/tipos/atualizar/{id}', ['as' => 'admin.tipos.atualizar', 'uses' => 'Admin\TipoController@atualizar']);
     Route::get('/admin/tipos/deletar/{id}', ['as' => 'admin.tipos.deletar', 'uses' => 'Admin\TipoController@deletar']);
+
+    Route::get('/admin/cidades', ['as' => 'admin.cidades', 'uses' => 'Admin\CidadeController@index']);
+    Route::get('/admin/cidades/adicionar', ['as' => 'admin.cidades.adicionar', 'uses' => 'Admin\CidadeController@adicionar']);
+    Route::post('/admin/cidades/salvar', ['as' => 'admin.cidades.salvar', 'uses' => 'Admin\CidadeController@salvar']);
+    Route::get('/admin/cidades/editar/{id}', ['as' => 'admin.cidades.editar', 'uses' => 'Admin\CidadeController@editar']);
+    Route::put('/admin/cidades/atualizar/{id}', ['as' => 'admin.cidades.atualizar', 'uses' => 'Admin\CidadeController@atualizar']);
+    Route::get('/admin/cidades/deletar/{id}', ['as' => 'admin.cidades.deletar', 'uses' => 'Admin\CidadeController@deletar']);
 });
